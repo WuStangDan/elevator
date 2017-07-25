44cc0dbf1f4e1c673e219edbccd3c9fb5d4212ca
Daniel <dstang14@gmail.com>
2017-07-24 20:55:49 -0400

    Add strip chart to simulator.

--- anim.py
+++ anim.py
@@ -1,15 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.gridspec as gridspec
+
 from scipy.integrate import odeint
 
-def update_plot(num, data, data2, line, line2):
+def update_plot(num, data, state, line, line2, line3):
     line.set_data(data[:,:int(num/10)])
     line2.set_data(20,state[num,0])
 
     time_text.set_text(str(num/100))
 
-    return line, line2, time_text,
+    line3.set_data(t[0:num], state[0:num,0])
+
+    return line, line2, time_text, line3
 
 
 
@@ -48,19 +52,31 @@ elevator[0] += el_start[0]
 elevator[1] += el_start[1]
 
 fig1 = plt.figure(figsize=(4,8))
+gs = gridspec.GridSpec(4,1)
 
-ax = fig1.add_subplot(111)
+# Elevator plot.
+ax = fig1.add_subplot(gs[:3, :])
 time_text = ax.text(.5, .5, '', fontsize=15)
-
-
-l, l2, = plt.plot([], [], 'r-', [], [], 'k^')
+l, l2, = ax.plot([], [], 'r-', [], [], 'k^')
 
 plt.xlim(0, 40)
 plt.ylim(0, 50)
 #plt.xlabel('x')
-plt.title('test')
+plt.title('Elevator')
 
-elevator_ani = animation.FuncAnimation(fig1, update_plot, int(5.02*100), fargs=(data, state, l, l2),
+
+# Strip chart.
+ax = fig1.add_subplot(gs[3,:])
+l3, = ax.plot([], [], '-b')
+
+
+plt.xlim(0, 10)
+plt.ylim(-50, 50)
+
+
+
+
+elevator_ani = animation.FuncAnimation(fig1, update_plot, int(5.02*100), fargs=(data, state, l, l2, l3),
     interval=10, repeat = False, blit=True)
 #line_ani.save('lines.mp4')
 
