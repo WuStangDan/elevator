fb706ef3e8e36a965fd41985d7d2d3caba265fc9
Daniel <dstang14@gmail.com>
2017-07-24 21:15:19 -0400

    Add controller interface.

--- anim.py
+++ anim.py
@@ -17,6 +17,13 @@ def update_plot(num, data, state, line, line2, line3):
 
 
 
+def PidController(x):
+    if x < 20:
+        return 30
+    else:
+        return 0
+
+
 def ElevatorPhysics(state, t):
     # State vector.
     x = state[0]
@@ -27,14 +34,14 @@ def ElevatorPhysics(state, t):
     # Acceleration of gravity.
     g = -9.8
 
-    x_dot_dot = g
+    x_dot_dot = g + PidController(x)
 
     # Output state derivatives.
     return [x_dot, x_dot_dot]
 
 
 state_initial = [50.0, 0.0]
-t = np.arange(0.0, 50.0, 0.01)
+t = np.arange(0.0, 110.0, 0.01)
 
 state = odeint(ElevatorPhysics, state_initial, t)
 
@@ -42,22 +49,14 @@ data = np.array([np.ones(51)*39, np.arange(51)])
 
 
 
-el_width = 10
-el_height = 15
-el_start = np.array([15, 35])
-elevator = np.array([[0, el_width, el_width, 0, 0],
-                      [0, 0, el_height, el_height, 0]])
-
-elevator[0] += el_start[0]
-elevator[1] += el_start[1]
 
 fig1 = plt.figure(figsize=(4,8))
-gs = gridspec.GridSpec(4,1)
+gs = gridspec.GridSpec(17,1)
 
 # Elevator plot.
-ax = fig1.add_subplot(gs[:3, :])
+ax = fig1.add_subplot(gs[:13, :])
 time_text = ax.text(.5, .5, '', fontsize=15)
-l, l2, = ax.plot([], [], 'r-', [], [], 'k^')
+l, l2, = ax.plot([], [], 'r-', [], [], 'ks', markersize = 50, markerfacecolor='None', markeredgewidth=3)
 
 plt.xlim(0, 40)
 plt.ylim(0, 50)
@@ -66,7 +65,7 @@ plt.title('Elevator')
 
 
 # Strip chart.
-ax = fig1.add_subplot(gs[3,:])
+ax = fig1.add_subplot(gs[14:19,:])
 l3, = ax.plot([], [], '-b')
 
 
@@ -76,7 +75,7 @@ plt.ylim(-50, 50)
 
 
 
-elevator_ani = animation.FuncAnimation(fig1, update_plot, int(5.02*100), fargs=(data, state, l, l2, l3),
+elevator_ani = animation.FuncAnimation(fig1, update_plot, int(10.02*100), fargs=(data, state, l, l2, l3),
     interval=10, repeat = False, blit=True)
 #line_ani.save('lines.mp4')
 
