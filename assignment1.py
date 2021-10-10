8c2cb7e425ede80054e9f81df026c96f5cae47e3
Honey <himanshushar@iisc.ac.in>
2021-10-10 19:02:26 +0530

    solution of assignment 1

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sim.elevator import sim_run
-
+import time
 # Simulator Options
 options = {}
 options['FIG_SIZE'] = [8, 8] # [Width, Height]
@@ -8,21 +8,21 @@ options['PID_DEBUG'] = False
 
 # Physics Options
 options['GRAVITY'] = True
-options['FRICTION'] = False
+options['FRICTION'] = True
 options['ELEVATOR_MASS'] = 500
-options['COUNTERWEIGHT_MASS'] = 0
+options['COUNTERWEIGHT_MASS'] = 500
 options['PEOPLE_MASS'] = 0
 
 # Controller Options
 options['CONTROLLER'] = True
-options['START_LOC'] = 27.0
-options['SET_POINT'] = 0.0
-options['OUTPUT_GAIN'] = 500
+options['START_LOC'] = 0.0
+options['SET_POINT'] = 3.0
+options['OUTPUT_GAIN'] = 1000
 
 
 class Controller:
     def __init__(self, reference):
-        self.r = reference
+        self.r = reference  # Set_Point value
         self.prev_time = 0
         self.output = 0
 
@@ -34,7 +34,22 @@ class Controller:
             self.prev_time = t
             # INSERT CODE BELOW.
             
-            self.output = 4
+            # Friction = velocity * coefficient
+            kp = 1
+            e = self.r - x
+            self.output = kp *e
+
+            '''
+            LOGIC CONTROLLER
+            ---------------------
+            if x < self.r:
+                self.output = 1
+            else:
+                self.output = -1
+            ---------------------
+            '''
+            
+
             # INSERT CODE ABOVE.
             return self.output
 
